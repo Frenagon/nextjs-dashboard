fba152060cbe93712e017c6b57ab8c25460f85d2
Francisco Aceves <frenagon@gmail.com>
2025-04-10 18:32:59 -0600

    config: add nix flake with dev env

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Basic nodejs template";
+
+  inputs.nixpkgs.url = "nixpkgs";
+
+  outputs = {nixpkgs, ...}: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+      });
+  in {
+    packages = forAllSystems (system: {});
+
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgsFor.${system};
+      in {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodePackages_latest.nodejs
+            nodePackages_latest.prettier
+            nodePackages_latest.typescript
+            nodePackages_latest.pnpm
+          ];
+        };
+      }
+    );
+  };
+}
